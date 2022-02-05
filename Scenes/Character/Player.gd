26caf118522a4195c87967eab4e3dacfe59204b2
ArnaudDelperier <arnaud.delperier@gmail.com>
2022-02-05 16:26:09 +0100

    first commit

---
+++ Scenes/Character/Player.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+onready var animated_sprite = get_node("AnimatedSprite")
+
+var speed = 300.0
+var direction = Vector2.ZERO
+
+
+func _process(delta: float) -> void:
+	move_and_slide(direction * speed)
+
+
+func _input(event: InputEvent) -> void:
+	direction.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	direction.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	direction = direction.normalized()	
+	
+	match(direction):
+		Vector2.RIGHT: animated_sprite.play("MoveRight")
+		Vector2.LEFT: animated_sprite.play("MoveLeft")
+		Vector2.UP: animated_sprite.play("MoveUp")
+		Vector2.DOWN: animated_sprite.play("MoveDown")
+		Vector2.ZERO:
+			animated_sprite.stop()
+			animated_sprite.set_frame(0)
